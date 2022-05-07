6a85f73f749ea2a5c0fc1923bde657bb848c17b5
subin195-09 <k_subin@naver.com>
2022-05-07 17:46:48 +0900

    :art: #1 Add Alarm Entity & Add Friends Entity

---
+++ src/entities/friends.entity.ts
@@ -0,0 +1,23 @@
+import {
+	Column, Entity, ManyToOne, PrimaryGeneratedColumn,
+  } from 'typeorm';
+  // eslint-disable-next-line import/no-cycle
+  import User from './users.entity';
+
+  @Entity()
+  export default class AlarmEntity {
+	@PrimaryGeneratedColumn()
+	  friendSeq: number;
+
+	@Column()
+	  isBlocked: boolean;
+
+	@Column()
+	  status: string;
+
+	@ManyToOne(() => User, (user) => user.userSeq)
+	  followerSeq: number;
+
+	@ManyToOne(() => User, (user) => user.userSeq)
+	  followeeSeq: number;
+  }
