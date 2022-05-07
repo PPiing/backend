102100849ba13e67768550016fa6a06696abbe32
JAWSP <lightload2912@gmail.com>
2022-05-07 22:40:47 +0900

    :bug: add pg..

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -12,6 +12,8 @@ export default class UserEntity {
     rankSeq: RankEntity;
   //여기서 게임로그로 유저의 시퀸스를 넘기는데
   //이겼는지 졌는지를 어캐 알지?
+  //플레이어1(본인), 플레이어2(상대), 점수비교해서 큰쪽으로 승패유무를 결정해야하나
+  //근데 그게 실제 구현이 되나
   @OneToMany(() => GameLog, (gamelog) => gamelog.winnerSeq)
   @OneToMany(() => GameLog, (gamelog) => gamelog.loserSeq)
     gameSeq: GameLog[];
