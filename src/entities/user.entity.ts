a665198d4673405da18847d175e88c3e64152ff2
jinbekim <jinbekim@student.42seoul.kr>
2022-05-11 17:17:16 +0900

    resolve conflict user entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,6 @@
-import { Entity, OneToMany, PrimaryColumn } from 'typeorm';
+import {
+  Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn,
+} from 'typeorm';
 import Alarm from './alarm.entity';
 import ChatParticipant from './chat-participant.entity';
 import Friends from './friends.entity';
@@ -6,8 +8,32 @@ import GameLog from './gamelog.entity';
 
 @Entity()
 export default class User {
-  @PrimaryColumn()
-    userSeq: number;
+  @PrimaryGeneratedColumn()
+    userSeq: number; // 유저 시퀀스
+
+  @Column()
+    userId: number; // 42 ID
+
+  @Column()
+    nickName: string; // 닉네임
+
+  @Column()
+    email: string; // 이메일
+
+  @Column()
+    secAuthStatuc: boolean; // 이차인증 여부
+
+  @Column({ nullable: true })
+    avatarImgUri: string; // 프로필 이미지 URI
+
+  @Column()
+    status: boolean; // 접속 여부
+
+  @Column()
+    deleteStatus: boolean; // 삭제 여부
+
+  @CreateDateColumn()
+    createdAt: Date; // 생성일
 
   @OneToMany(() => Alarm, (alarm) => alarm.receiverSeq)
     receiver: Alarm[];
