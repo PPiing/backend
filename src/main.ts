3942f549ab295c1eb87c822795be24b32283bd3d
jinbekim <jinbekim@student.42seoul.kr>
2022-05-13 20:21:01 +0900

    Fix: improper import in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import * as session from 'express-session';
 import * as passport from 'passport';
 import { ConfigService } from '@nestjs/config';
-import AppModule from './app.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
