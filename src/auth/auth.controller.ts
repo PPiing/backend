bde8e1f39db5cc229eceec42ad1873ac0de5ef9e
jinbekim <jinbekim@student.42seoul.kr>
2022-05-13 20:10:24 +0900

    feat: :sparkles: Add: register user automatically when login with oauth.
    #16

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import {
   Controller, Get, Logger, Redirect, Req, UseGuards,
 } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 import { FtGuard } from './guards/ft.guard';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly userService: UserService) { }
+
   @Get('42')
   @UseGuards(FtGuard)
   login() {
@@ -14,5 +17,10 @@ export class AuthController {
   @UseGuards(FtGuard)
   @Redirect('/', 302)
   callback(@Req() req: any) {
+    // sign up user
+    const [userId, email] = [req.user.userId, req.user.email];
+    const result = this.userService.createByUserId(userId, email);
+    Logger.debug(result);
+    return result;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { FtStrategy } from './ft.strategy';
 import { SessionSerializer } from './session.serializer';
 
 @Module({
+  imports: [UserModule],
   controllers: [AuthController],
   providers: [SessionSerializer, FtStrategy],
 })
