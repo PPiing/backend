ac436ac1511a69162d5ed36fcda8324f3bc2de0f
jinbekim <jinbekim@student.42seoul.kr>
2022-06-07 17:18:03 +0900

    Add: GameData DTO

---
+++ src/game-log/dto/meta.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsBoolean, IsEnum, IsObject, IsString,
+} from 'class-validator';
+import { GameType } from 'src/enums/mastercode/game-type.enum';
+import { GameSession } from '../interface/game-session';
+
+export class MetaData {
+  constructor(
+    roomId: string,
+    playerTop: GameSession,
+    playerBtm: GameSession,
+    gameType: GameType,
+  ) {
+    this.roomId = roomId;
+    this.playerTop = playerTop;
+    this.playerBtm = playerBtm;
+    this.gameType = gameType;
+  }
+
+  gameSeq: number;
+
+  @IsString()
+    roomId: string;
+
+  @IsObject()
+    playerTop: GameSession;
+
+  @IsObject()
+    playerBtm: GameSession;
+
+  @IsString()
+    gameType: GameType;
+}
