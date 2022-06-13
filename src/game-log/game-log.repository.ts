4283e1049547e2175e07b8c7e4c9bdd54978ebb3
jinbekim <jinbekim@student.42seoul.kr>
2022-06-13 10:09:39 +0900

    :clown_face: Add: Mock game data

--- src/game-log/game-log.repository.ts
+++ src/game-log/game-log.repository.ts
@@ -1,5 +1,73 @@
+/* eslint-disable max-classes-per-file */
 import GameLog from 'src/entities/game-log.entity';
+import GameOption from 'src/enums/mastercode/game-option.enum';
+import GameType from 'src/enums/mastercode/game-type.enum';
 import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(GameLog)
 export class GameLogRepository extends Repository<GameLog> {}
+
+export class MockGameLogRepository {
+  constructor(private logs: GameLog[]) {
+    this.logs.push(
+      {
+        gameLogSeq: 1,
+        gameSeq: 1,
+        gameType: GameType.NORMAL,
+        winnerSeq: 1,
+        topSideScore: 5,
+        btmSideScore: 2,
+        option1: GameOption.GLOP10,
+        option2: GameOption.GLOP20,
+        option3: GameOption.GLOP40,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameLogSeq: 2,
+        gameSeq: 2,
+        gameType: GameType.NORMAL,
+        winnerSeq: 2,
+        topSideScore: 3,
+        btmSideScore: 5,
+        option1: GameOption.GLOP10,
+        option2: GameOption.GLOP20,
+        option3: GameOption.GLOP40,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameLogSeq: 3,
+        gameSeq: 3,
+        gameType: GameType.NORMAL,
+        winnerSeq: 4,
+        topSideScore: 1,
+        btmSideScore: 5,
+        option1: GameOption.GLOP10,
+        option2: GameOption.GLOP20,
+        option3: GameOption.GLOP40,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    );
+  }
+
+  /**
+   * @return all game logs
+   */
+  async find(): Promise<GameLog[]> {
+    return this.logs;
+  }
+
+  /**
+   * @return game log
+   */
+  async findOneBySeq(seq: number): Promise<GameLog> {
+    return this.logs.find((val) => val.gameSeq === seq);
+  }
+
+  async save(log: GameLog): Promise<GameLog | void> {
+    this.logs.push(log);
+    return log;
+  }
+}
--- src/game-log/game.repository.ts
+++ src/game-log/game.repository.ts
@@ -1,5 +1,58 @@
+/* eslint-disable max-classes-per-file */
+
 import { Game } from 'src/entities/game.entity';
 import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(Game)
 export class GameRepository extends Repository<Game> {}
+
+export class MockGameRepository {
+  constructor(private logs: Game[]) {
+    this.logs.push(
+      {
+        gameSeq: 1,
+        roomId: 'room1',
+        topUserName: 'user1',
+        btmUserName: 'user2',
+        topUserSeq: 1,
+        btmUserSeq: 2,
+        createdAt: new Date(),
+        finishedAt: new Date(),
+      },
+      {
+        gameSeq: 2,
+        roomId: 'room2',
+        topUserName: 'user2',
+        btmUserName: 'user3',
+        topUserSeq: 3,
+        btmUserSeq: 4,
+        createdAt: new Date(),
+        finishedAt: new Date(),
+      },
+      {
+        gameSeq: 3,
+        roomId: 'room4',
+        topUserName: 'user4',
+        btmUserName: 'user5',
+        topUserSeq: 4,
+        btmUserSeq: 5,
+        createdAt: new Date(),
+        finishedAt: new Date(),
+      },
+    );
+  }
+
+  async find(): Promise<Game[] | void> {
+    return this.logs;
+  }
+
+  async findOneBySeq(seq: number): Promise<Game | void> {
+    const game = await this.logs.find((val) => val.gameSeq === seq);
+    return game;
+  }
+
+  async save(game: Game): Promise<Game | void> {
+    this.logs.push(game);
+    return game;
+  }
+}
