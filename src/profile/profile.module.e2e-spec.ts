482389f862853064bbdc326a346d36d81576b1c5
subin195-09 <k_subin@naver.com>
2022-07-07 01:57:13 +0900

    :bug: test:e2e fix #73

--- src/profile/profile.module.e2e-spec.ts
+++ src/profile/profile.module.e2e-spec.ts
@@ -4,17 +4,31 @@ import { ProfileController } from './profile.controller';
 import { UserProfileService } from './user-profile.service';
 import MockUserProfileRepository from './repository/mock/mock.user-profile.repository';
 import { UserProfileRepository } from './repository/user-profile.repository';
+import { UserAchivRepository } from './repository/user-achiv.repository';
+import MockUserAchivRepository from './repository/mock/mock.user-achiv.repository';
+import { AchivRepository } from './repository/achiv.repository';
+import MockAchivRepository from './repository/mock/mock.achiv.repository';
+import { UserAchivService } from './user-achiv.service';
 
 const repositories = [
   {
     provide: getRepositoryToken(UserProfileRepository),
     useClass: MockUserProfileRepository,
   },
+  {
+    provide: getRepositoryToken(UserAchivRepository),
+    useClass: MockUserAchivRepository,
+  },
+  {
+    provide: getRepositoryToken(AchivRepository),
+    useClass: MockAchivRepository,
+  },
 ];
 @Module({
   controllers: [ProfileController],
   providers: [
     UserProfileService,
+    UserAchivService,
     ...repositories,
   ],
   exports: [UserProfileService],
