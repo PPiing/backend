19f8bd4425e0a242b8795cbb21b8b03096c05c7b
joohongpark <daleaderprofq@gmail.com>
2022-07-09 18:58:08 +0900

    fix: circular defedency problem

---
+++ src/game-log/game.module.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { GameGateway } from './game.gateway';
+import { GameService } from './game.service';
+import { GameLogRepository } from './repository/game-log.repository';
+import { GameQueue } from './game-queue';
+import { SimulationService } from './simulation.service';
+import { GameSocketSession } from './game-socket-session';
+import { GameLogController } from './game-log.controller';
+import { GameLogService } from './game-log.service';
+import { MockGameLogRepository } from './repository/mock/mock.game-log.repository';
+import AppModule from 'src/app.module.e2e-spec';
+
+@Module({
+  imports: [
+    forwardRef(() => AppModule),
+    EventEmitterModule.forRoot(),
+  ],
+  providers: [
+    GameService,
+    GameGateway,
+    GameQueue,
+    {
+      provide: getRepositoryToken(GameLogRepository),
+      useClass: MockGameLogRepository,
+    },
+    SimulationService,
+    GameSocketSession,
+    GameLogService,
+  ],
+  controllers: [GameLogController],
+  exports: [GameGateway, GameLogService],
+})
+export class GameModule {}
