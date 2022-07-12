e2988e4dffc0f53e7368f1b5f0e411939e95d4c8
joohongpark <daleaderprofq@gmail.com>
2022-07-12 15:28:16 +0900

    fix: env settings

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ module.exports = {
   type: 'postgres',
   host: process.env.DATABASE_HOST,
   port: parseInt(process.env.DATABASE_PORT, 10),
-  username: process.env.DATABASE_USER,
+  username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   synchronize: true,
