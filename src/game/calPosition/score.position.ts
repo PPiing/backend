74286ba16848d7610a7b1e6c36870c2e2776224a
jinbekim <jinbekim@student.42seoul.kr>
2022-07-12 13:52:59 +0900

    add: check end of game function

--- src/game/calPosition/score.position.ts
+++ src/game/calPosition/score.position.ts
@@ -25,3 +25,25 @@ export const checkScorePosition = (game: GameData) : ScorePosition => {
   }
   return ScorePosition.playing;
 };
+
+/**
+ * checkEndOfGame 의 return type
+ */
+export const enum GameResult {
+  blueWin = 'blueWin',
+  redWin = 'redWin',
+  playing = 'playing',
+}
+
+/**
+ * 매치스코어와 현재 점수를 비교해서 게임이 종료 되는지 판단한다.
+ * @param game game data
+ * @returns game result
+ */
+export const checkEndOfGame = (game: GameData) : GameResult => {
+  const { inGameData: { matchScore, scoreBlue, scoreRed } } = game;
+
+  if (matchScore === scoreBlue) return GameResult.blueWin;
+  if (matchScore === scoreRed) return GameResult.redWin;
+  return GameResult.playing;
+};
