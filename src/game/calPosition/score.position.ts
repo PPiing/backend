394d45b7a3bf60cbc51464619cbacab49fb0974c
jinbekim <jinbekim@student.42seoul.kr>
2022-07-12 11:28:59 +0900

    add: ScorePosition enum: for scoring

--- src/game/calPosition/score.position.ts
+++ src/game/calPosition/score.position.ts
@@ -1,23 +1,27 @@
 import { GameData } from '../dto/game-data';
 
+export const enum ScorePosition {
+  blueWin = 'blueWin',
+  redWin = 'redWin',
+  playing = 'playing',
+}
 /**
  * 매 프레임 마다 공의 위치가 승리조건에 부합하는지 판단한다.
  * @param game game data
  * @returns win or lose
  */
-export const checkScorePosition = (game: GameData) => {
-  const { inGameData } = game;
-  const { ball } = inGameData;
+export const checkScorePosition = (game: GameData) : ScorePosition => {
+  const { inGameData, inGameData: { ball } } = game;
 
   // left(blue) lose
   if (ball.position.x - GameData.spec.ball.radius < ((GameData.spec.arena.width / 2) * -1)) {
     inGameData.scoreBlue += 1;
-    return 1;
+    return ScorePosition.blueWin;
   }
   // rigth(red) lose
   if (ball.position.x + GameData.spec.ball.radius > (GameData.spec.arena.width / 2)) {
     inGameData.scoreRed += 1;
-    return 2;
+    return ScorePosition.redWin;
   }
-  return false;
+  return ScorePosition.playing;
 };
