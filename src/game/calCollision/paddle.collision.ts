0146be6dab98fbcdbb710f184b84cc7531a0eed3
jinbekim <jinbekim@student.42seoul.kr>
2022-07-15 10:22:37 +0900

    refac: checkpaddlecollision function return boolean whether ball is bound paddle or not

--- src/game/calCollision/paddle.collision.ts
+++ src/game/calCollision/paddle.collision.ts
@@ -1,29 +1,26 @@
 import { GameData } from '../dto/game-data';
 
 /**
- * 패들과의 충돌을 판단해서 방향을 바꿔준다.
+ * 패들과의 충돌을 판단한다.
  * @param game game data
+ * @return boolean
  */
-export const checkPaddleCollision = (game: GameData) => {
-  const { inGameData } = game;
-  const { ball, paddleBlue, paddleRed } = inGameData;
-  const BALL = ball.position;
-  const SPEC = GameData.spec;
-  if (
-    BALL.x < paddleBlue.position.x + SPEC.paddle.width / 2
-    && BALL.x > paddleBlue.position.x - SPEC.paddle.width / 2
-    && BALL.y - SPEC.ball.radius < paddleBlue.position.y + SPEC.paddle.height / 2
-    && BALL.y + SPEC.ball.radius > paddleBlue.position.y - SPEC.paddle.height / 2
-  ) {
-    ball.velocity.y *= -1;
-    BALL.y = paddleBlue.position.y + SPEC.ball.radius + SPEC.paddle.height / 2;
-  } else if (
-    BALL.x - SPEC.ball.radius < paddleRed.position.x + SPEC.paddle.width
-    && BALL.x + SPEC.ball.radius > paddleRed.position.x
-    && BALL.y + SPEC.ball.radius > paddleRed.position.y
-    && BALL.y - SPEC.ball.radius < paddleRed.position.y + SPEC.paddle.height
-  ) {
-    ball.velocity.y *= -1;
-    BALL.y = paddleRed.position.y - SPEC.ball.radius - SPEC.paddle.height / 2;
-  }
+export const checkPaddleCollision = (game: GameData): boolean => {
+  const { inGameData: { ball, paddleBlue, paddleRed } } = game;
+  const HALF_OF_PADDLE_HEIGHT = GameData.spec.paddle.height / 2;
+  const HALF_OF_PADDLE_WIDTH = GameData.spec.paddle.width / 2;
+  const BALL = GameData.spec.ball;
+
+  /** Check bluePaddle collision */
+  if (ball.position.x + BALL.radius < paddleBlue.position.x + HALF_OF_PADDLE_WIDTH
+    && ball.position.x + BALL.radius > paddleBlue.position.x - HALF_OF_PADDLE_WIDTH
+    && ball.position.y < paddleBlue.position.y + HALF_OF_PADDLE_HEIGHT
+    && ball.position.y > paddleBlue.position.y - HALF_OF_PADDLE_HEIGHT) { return true; }
+  /** Check bluePaddle collision */
+  if (ball.position.x + BALL.radius < paddleRed.position.x + HALF_OF_PADDLE_WIDTH
+    && ball.position.x + BALL.radius > paddleRed.position.x - HALF_OF_PADDLE_WIDTH
+    && ball.position.y < paddleRed.position.y + HALF_OF_PADDLE_HEIGHT
+    && ball.position.y > paddleRed.position.y - HALF_OF_PADDLE_HEIGHT) { return true; }
+
+  return false;
 };
