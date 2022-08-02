ed72a6608a29e764ac5d78a69f9d782389a8ec14
joohongpark <daleaderprofq@gmail.com>
2022-08-03 01:23:07 +0900

    fix: lint

--- src/chatrooms/chatrooms.module.e2e-spec.ts
+++ src/chatrooms/chatrooms.module.e2e-spec.ts
@@ -3,6 +3,7 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ScheduleModule } from '@nestjs/schedule';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import AppModule from 'src/app.module.e2e-spec';
+import { UserModule } from 'src/user/user.module.e2e-spec';
 import ChatParticipantRepository from './repository/chat-participant.repository';
 import ChatRepository from './repository/chat.repository';
 import ChatroomsController from './chatrooms.controller';
@@ -16,7 +17,6 @@ import MockMessageRepository from './repository/mock/mock.message.repository';
 import MockChatParticipantRepository from './repository/mock/mock.chat-participant.repository';
 import MockFriendsRepository from './repository/mock/mock.friends.repository';
 import ChatroomsService from './chatrooms.service';
-import { UserModule } from 'src/user/user.module.e2e-spec';
 
 const repositories = [
   {
