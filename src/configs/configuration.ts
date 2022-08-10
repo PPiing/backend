8030a3fb29d0a00849ec600fd216ed52d0eb7be0
jinbekim <jinbekim@student.42seoul.kr>
2022-08-10 16:26:15 +0900

    revert configuration

--- src/configs/configuration.ts
+++ src/configs/configuration.ts
@@ -5,13 +5,13 @@ export default () => ({
     host: process.env.DATABASE_HOST || 'localhost',
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
     username: process.env.DATABASE_USERNAME || 'user',
-    password: process.env.DATABASE_PASSWORD || 'pass1234',
-    database: process.env.DATABASE_NAME || 'ppoong',
+    password: process.env.DATABASE_PASSWORD || 'example',
+    database: process.env.DATABASE_NAME || 'db',
   },
   auth: {
     secret: process.env.AUTH_SECRET || 'secret',
-    clientid: process.env.AUTH_CLIENTID || '7c90708f53bc6b84eeb85efda836831fd9fb6b9f43b724eafe68f501a2424b14',
-    clientsecret: process.env.AUTH_CLIENTSECRET || '50db10753c8cb41a0cfbdbfce303d394eef2db28509aaf84002db1c09ee1c353',
+    clientid: process.env.AUTH_CLIENTID,
+    clientsecret: process.env.AUTH_CLIENTSECRET,
   },
   mail: {
     user: process.env.MAILDEV_INCOMING_USER,
