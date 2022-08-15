cb231b4083fb5393a14f2f592055adaa5cc96975
jinbekim <jinbekim@student.42seoul.kr>
2022-08-15 20:18:50 +0900

    queue find with userSeq

--- src/game/game-queue.ts
+++ src/game/game-queue.ts
@@ -10,8 +10,7 @@ export class GameQueue {
   private ladderQueue: Array<[any, RuleDto]> = [];
 
   enQueue(client: any, enqueueData: RuleDto) {
-    this.logger.debug(client);
-    console.log(client, enqueueData);
+    this.logger.debug('enqueue', client.request.user);
     const { isRankGame } = enqueueData;
 
     if (isRankGame === true) {
@@ -21,7 +20,7 @@ export class GameQueue {
   }
 
   deQueue(client:any, dequeueData: RuleDto) {
-    this.logger.debug(client);
+    this.logger.debug('dequeue', client.request.user);
     const { isRankGame } = dequeueData;
 
     if (isRankGame === true) {
@@ -32,14 +31,14 @@ export class GameQueue {
 
   private async addToNormalQueue(client: any, enqueueData: RuleDto) {
     this.logger.debug('try to add to normal queue');
-    const dup = this.normalQueue.find((tuple) => tuple[0] === client && tuple[1] === enqueueData);
+    const dup = this.normalQueue.find(
+      (tuple) => tuple[0].request.user.userSeq === client.request.user.userSeq,
+    );
     if (!dup) {
-      this.logger.debug('addto Nomal', client, 'queue', this.normalQueue);
       this.normalQueue.push([client, enqueueData]);
     }
     if (this.normalQueue.length >= 2) {
       const players: [any, RuleDto][] = this.normalQueue.splice(0, 2);
-      console.log(players);
       return players;
     }
     return false;
@@ -47,9 +46,8 @@ export class GameQueue {
 
   private async addToLadderQueue(client: any, enqueueData: RuleDto) {
     this.logger.debug('try to add to ladder queue');
-    const dup = this.ladderQueue.find((tuple) => tuple[0] === client && tuple[1] === enqueueData);
+    const dup = this.ladderQueue.find((tuple) => tuple[0] === client.request.user.userSeq);
     if (!dup) {
-      this.logger.debug('addto Ladder', client, 'queue', this.ladderQueue);
       this.ladderQueue.push([client, enqueueData]);
     }
     if (this.ladderQueue.length >= 2) {
@@ -60,14 +58,18 @@ export class GameQueue {
   }
 
   removeFromNormalQueue(client: any, dequeueData: RuleDto) {
-    const index = this.normalQueue.indexOf([client, dequeueData]);
-    if (index > -1) this.normalQueue.splice(index, 1);
+    const dup = this.normalQueue.findIndex(
+      (tuple) => tuple[0].request.user.userSeq === client.request.user.userSeq,
+    );
+    if (!dup) this.normalQueue.splice(dup, 1);
     else this.logger.error(`${client.userId} is not in normalQueue queue`);
   }
 
   removeFromLadderQueue(client: any, dequeueData: RuleDto) {
-    const index = this.ladderQueue.indexOf([client, dequeueData]);
-    if (index > -1) this.ladderQueue.splice(index, 1);
+    const dup = this.ladderQueue.findIndex(
+      (tuple) => tuple[0].request.user.userSeq === client.request.user.userSeq,
+    );
+    if (!dup) this.ladderQueue.splice(dup, 1);
     else this.logger.error(`${client.userId} is not in ladderQueue queue`);
   }
 }
